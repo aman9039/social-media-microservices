e4ce9538488f5999fdb92ca4f118def5ed40a1be
amangupta <amangupta9039093489@gmail.com>
2025-06-05 10:21:31 +0530

    correct logger error

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -40,7 +40,8 @@ app.use(rateLimitOptions);
 
 app.use((req, res, next) => {
   logger.info(`Received ${req.method} request to ${req.url}`);
-  logger.info(`Request body, ${req.body}`);
+logger.info(`Request body: ${JSON.stringify(req.body)}`);
+
   next();
 });
 
